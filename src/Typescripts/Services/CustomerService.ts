a19d0ff7d99c6ffe58efe89c09f6560f47182922
dariodds <dario.dasilva.private@gmail.com>
2025-04-01 14:04:07 +0200

    add SweetAlert lib

--- src/Typescripts/Services/CustomerService.ts
+++ src/Typescripts/Services/CustomerService.ts
@@ -1,15 +1,15 @@
 import {Customer} from "../Entities/Customer";
 import * as log from 'N/log';
 
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 export class CustomerService {
   displayAlert(title: string, message: string) {
     log.debug('CustomerService', 'displayAlert');
-    // Swal.fire({
-    //   title: title,
-    //   text: message,
-    //   icon: "success"
-    // });
+    Swal.fire({
+      title: title,
+      text: message,
+      icon: "success"
+    });
   }
 }
